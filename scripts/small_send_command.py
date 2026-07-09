876c74ef7c541409c874f8740f0df89b55adfef4
Vladimír Ulman <ulman@fi.muni.cz>
2026-07-09 22:24:04 +0200

    ADD: example_full_record.json from py package example, plus...
    its ccmm-enhanced variant
    
    Invenio (oarepo 4.0.0) DOES ACCEPT this payload example

--- scripts/small_send_command.py
+++ scripts/small_send_command.py
@@ -5,7 +5,7 @@ token = "jd2tX8I4evotMo3jjdJxU2pHM0ZTCMn0JuqkIVU6qzF5rlElG5ACMRV8Wh0o"
 
 url = "https://127.0.0.1:5000"
 h = {"Accept": "application/json", "Content-Type": "application/json", "Authorization": f"Bearer {token}"}
-datapath = "/home/ulman/devel/EOSC/czbirepo/model/example_payload/example_small.json"
+datapath = "/home/ulman/devel/EOSC/czbirepo/model/example_payload/example_full_record.json"
 
 with open(datapath) as fp:
     data = json.load(fp)
